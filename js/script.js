a334838a33dbcf37df3ff9804d80ada39af5face
Lawrence <lawrence.ho@mail.mcgill.ca>
2020-05-27 11:17:18 -0400

    first local commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+function toggle(){
+	var msg = document.getElementById("hiddenMsg");
+	if(msg.style.display!="block"){
+		msg.style.display="block";
+	} 
+	else {
+		msg.style.display="none";
+	}
+	//alert(`testing 1`);
+	//alert(`testing ${1}`);
+	result = prompt(text to show, [default]);
+}
+
+console.log('JS: I am working')
